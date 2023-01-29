e526bb85ac05bf526917bd17435859d9ab849cf2
MaartenvdGoor <m.van.de.goor@sintlucasedu.nl>
2023-01-29 17:31:27 +0100

    Init Commit

---
+++ ASP_Portfolio/Services/JsonFileProjectService.cs
@@ -0,0 +1,33 @@
+﻿using ASP_Portfolio.Models;
+using System.Collections;
+using System.Text.Json;
+
+namespace ASP_Portfolio.Services
+{
+	public class JsonFileProjectService
+	{
+		public JsonFileProjectService(IWebHostEnvironment webHostEnvironment)
+		{
+			WebHostEnvironment = webHostEnvironment;
+		}
+
+		public IWebHostEnvironment WebHostEnvironment { get; }
+
+		private string JsonFileName {
+			get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Projects.json"); }
+		}
+
+		public IEnumerable<Project>? GetProducts()
+		{
+			using (var jsonFileReader = File.OpenText(JsonFileName))
+			{
+				return JsonSerializer.Deserialize<Project[]>(jsonFileReader.ReadToEnd(),
+					new JsonSerializerOptions
+					{
+						PropertyNameCaseInsensitive = true
+					});
+			}
+			
+		}
+	} 
+}
